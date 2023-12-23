3772ea58c52741f75d837bbebfa219bd3244993a
Francisco Lopez <82164792+franciscols2011@users.noreply.github.com>
2023-12-23 02:48:43 +0000

    Todolist with react

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [todos, setTodos] = useState([]);
+  const [newItem, setNewItem] = useState("");
+
+  const manejoInput = (e) => {
+    setNewItem(e.target.value);
+  };
+
+  const agregarElemento = (e) => {
+    if (e.key === "Enter" && newItem.trim() !== "") {
+      setTodos([...todos, newItem]);
+      setNewItem("");
+    }
+  };
+
+  const eliminarElemento = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="todo-list">
+      <h1>todos 🎅</h1>
+      <input
+        type="text"
+        placeholder="Agrega una tarea"
+        value={newItem}
+        onChange={manejoInput}
+        onKeyDown={agregarElemento}
+      />
+
+      {todos.length > 0
+        ? todos.map((todo, index) => (
+            <div key={index} className="todo-item">
+              {todo}
+              <span
+                className="eliminar"
+                onClick={() => eliminarElemento(index)}
+              >
+                X
+              </span>
+            </div>
+          ))
+        : "No hay tareas, añadir tareas"}
+      <div>🌟 {todos.length} item left</div>
+    </div>
+  );
 };
 
 export default Home;
